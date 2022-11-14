73fe4f5721f5e51f03d8d8be2e991631ae03faee
knar <zakjsimpson@gmail.com>
2022-11-14 12:50:24 -0500

    Store foreign ObjectIds instead of strings

--- server/src/api/runs.js
+++ server/src/api/runs.js
@@ -1,16 +1,57 @@
 import express from 'express'
 import authenticate from '../middleware/authenticate.js'
+import { ObjectId } from 'mongodb'
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
     const collection = req.app.locals.db.collection('runs')
-    const found = await collection.find()
-    if (!found) {
+
+    let runs = await collection.aggregate([
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'uid',
+                foreignField: 'uid',
+                as: 'user'
+            },
+        },
+        {
+            $unwind: '$user'
+        },
+        {
+            $lookup: {
+                from: 'scenarios',
+                localField: 'scenId',
+                foreignField: '_id',
+                as: 'scenario'
+            }
+        },
+        {
+            $unwind: '$scenario'
+        },
+        {
+            $addFields: {
+                displayName: '$user.settings.displayName',
+                scenarioName: '$scenario.name',
+            }
+        },
+        {
+            $project: {
+                displayName: true,
+                scenarioName: true,
+                finTime: true,
+                stats: true,
+                _id: false,
+            }
+        }
+    ])
+
+    if (!runs) {
         res.sendStatus(404)
     }
 
-    const runs = await found.toArray()
+    runs = await runs.toArray()
     res.status(200).json(runs)
 })
 
@@ -18,7 +59,7 @@ router.post('/', authenticate, async (req, res) => {
     const collection = req.app.locals.db.collection('runs')
     collection.insertOne({
         uid: req.uid,
-        scenId: req.body.scenId,
+        scenId: new ObjectId(req.body.scenId),
         finTime: req.body.finTime,
         stats: req.body.stats,
     })
